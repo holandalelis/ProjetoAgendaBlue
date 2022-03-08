26c8607649da2ece54dc1e805b339c29bc49b953
pedro <pedrolelis@hotmail.com>
2022-03-08 00:34:45 -0300

    fim

--- agenda_front/src/App.vue
+++ agenda_front/src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Contatos msg="Bem Vindo(a) a Agenda da Blue Technology"/>
+  <div class="container" id="app">
+    <div class="nav-scroller py-1 mb-2">
+      <nav class="nav d-flex justify-content-between">
+        <a class="p-2 text-muted text black font italic" href="/">Agenda Blue Technology</a>
+        <a class="p-2 text-muted text black font italic" href="https://www.linkedin.com/company/blue-technology-brasil/">Blue Technology</a>
+      </nav>
+    </div>
+  </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Contatos msg="Bem Vindo(a) a Agenda da Blue Technology"/>
 </template>
 
 <script>
--- agenda_front/src/components/Contatos.vue
+++ agenda_front/src/components/Contatos.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <table>
+    <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+      <div class="col-md-6 px-0">
+          <h1 class="display-4 font italic">{{ msg }}</h1>
+      </div>
+    </div>
+    <form class="container">
+      <div class="form-groupr">
+        <label for="nome">Nome</label>
+        <input type="hidden" id="id" name="id">
+        <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o Nome">
+      </div>
+      <div class="form-group">
+        <label for="telefone">Telefone</label>
+        <input type="text" class="form-control" id="telefone" name="telefone" placeholder="Digite o Telefone">
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" class="form-control" id="email" name="email" placeholder="Digite o Email">
+      </div>
+      <button v-on:click="salvar()" type="button" class="btn btn-primary">Enviar</button>
+      <div style="color: red">
+        {{mensagem}}
+      </div>
+    </form>
+    <hr>
+    <table class="table">
       <thead>
         <tr>
           <th>
@@ -15,6 +39,9 @@
           </th>
           <th>
             Email
+          </th>
+          <th colspan="2">
+
           </th>
         </tr>
       </thead>
@@ -32,6 +59,12 @@
           <td>
             {{contato.email}}
           </td>
+          <td>
+            <button class="btn btn-primary" v-on:click="editar(contato)">Editar</button>
+          </td>
+          <td>
+            <button class="btn btn-danger" v-on:click="excluir(contato.id)">Excluir</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -48,18 +81,42 @@ export default {
   data: () => {
     return {
       contatos: [],
+      contato: undefined,
+      mensagem: ""
     }
   },
   methods: {
-    lista: (scope) => {
+    lista(){
       axios.get('https://localhost:44387/contatos').then((res)=>{
-      console.log(res)
-      scope.contatos = res.data
+        this.contatos = res.data
       })
+    },
+    salvar(){
+      axios.post('https://localhost:44387/contatos',
+      {
+        nome:document.getElementById("nome").value,
+        telefone:document.getElementById("telefone").value,
+        email:document.getElementById("email").value
+      }).then(()=>{
+          this.lista()
+      })
+    },
+    excluir() {
+      if (confirm("Confirma a Exclusão?")) {
+        axios.delete('https://localhost:44387/contatos/${id}').then(()=>{
+          this.lista()
+      })
+    }
+    },
+    editar(contato) {
+      this.contato.nome= document.getElementById("nome").value
+      this.contato.telefone= document.getElementById("telefone").value= contato.telefone
+      this.contato.email= document.getElementById("email").value= contato.email
+      this.contato = contato
     }
   },
   created(){
-    this.lista(this)
+    this.lista()
   }
 }
 </script>
